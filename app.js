4129d3137e7450d20ba3fff2d57e62ce4cb55af8
Gabriel Zimmermann <gabriel.zimmermann.dev@gmail.com>
2023-12-18 20:12:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var questions = [
+    {
+      "question": "Who was president before Barack Obama?",
+      "answers": ["Donald Trump", "George Bush", "Jimmy Carter", "Bill Clinton"],
+      "rightAnswer": "George Bush"
+    },
+    {
+      "question": "In which city are you if you are standing on the Spanish Steps?",
+      "answers": ["Madrid", "Paris", "London", "Rome"],
+      "rightAnswer": "Rome"
+    },
+    {
+      "question": "In what year was the UNO created?",
+      "answers": ["1945", "1952", "1938", "1965"],
+      "rightAnswer": "1945"
+    }
+  ];
+  
+  var questionBtn = document.querySelectorAll('.answer-btn');
+  var plyrScore = 0;
+  var questionIndex = 0;
+  
+  generateQuestion();
+  
+  function generateQuestion() {
+    if (questionIndex >= questions.length) {
+      // let parendElmResult = document.getElementById('result');
+  
+      // scoreText.appendChild(praragraph)
+      showResults()
+    }
+  
+    var currentQuestion = questions[questionIndex];
+  
+    document.getElementById('question-text').innerHTML = currentQuestion.question;
+  
+    for (let i = 0; i < currentQuestion.answers.length; i++) {
+      questionBtn[i].innerHTML = currentQuestion.answers[i];
+    }
+  
+    questionIndex++;
+  }
+  
+  questionBtn.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+      var btnClicked = event.target.innerHTML;
+      var currentQuestion = questions[questionIndex - 1];
+  
+      if (btnClicked === currentQuestion.rightAnswer) {
+        plyrScore++;
+        console.log(plyrScore);
+      } 
+  
+      generateQuestion();
+    });
+  });
+
+
+function showResults(){
+  let parendElmResult = document.getElementById("result");
+  
+  scoreText.createElement("p");
+  console.log('hello world');
+}
